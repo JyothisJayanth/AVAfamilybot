465e88849d84d331b3183a4b1d16d7cfdcad2453
JyothisJayanth <65120517+JyothisJayanth@users.noreply.github.com>
2020-05-14 09:43:39 +0530

    Update bans.py

--- tg_bot/modules/bans.py
+++ tg_bot/modules/bans.py
@@ -91,7 +91,7 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
         chat.kick_member(user_id)
         bot.send_sticker(chat.id, BAN_STICKER)  # banhammer marie sticker
         keyboard = []
-        reply = "{} Banned!".format(mention_html(member.user.id, member.user.first_name))
+        reply = "{} has been BANNED!".format(mention_html(member.user.id, member.user.first_name))
         message.reply_text(reply, reply_markup=keyboard, parse_mode=ParseMode.HTML)
         return log
 
