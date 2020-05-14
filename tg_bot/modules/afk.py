33adc00ed5a5ee254e179db2844c51ab5aa5ead9
JyothisJayanth <65120517+JyothisJayanth@users.noreply.github.com>
2020-05-14 10:03:17 +0530

    Update afk.py

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -58,13 +58,17 @@ def reply_afk(bot: Bot, update: Update):
             else:
                 return
 
-            if sql.is_afk(user_id):
-                user = sql.check_afk_status(user_id)
-                if not user.reason:
-                    res = "{} is away from the keyboard ! reason :\n{} ".format(fst_name)
-                else:
-                    res = "{} is away from the keyboard ! reason :\n{}. ".format(fst_name, user.reason)
-                message.reply_text(res)
+    elif message.reply_to_message:
+        user_id =  message.reply_to_message.from_user.id
+        fst_name = message.reply_to_message.from_user.first_name
+
+    if sql.is_afk(user_id):
+        user = sql.check_afk_status(user_id)
+        if not user.reason:
+            res = tld(update.effective_chat.id, "{} is AFK!").format(fst_name)
+        else:
+            res = tld(update.effective_chat.id, "{} is AFK! says its because of:\n{}").format(fst_name, user.reason)
+            message.reply_text(res)
 
 
 __help__ = """
